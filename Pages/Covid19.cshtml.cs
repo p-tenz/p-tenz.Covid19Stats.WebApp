e8619fc7439d3b4b4ae9dbd38519838ed3503dd7
Petra Tenzlinger <petra@tenzlinger.de>
2020-10-23 14:36:58 +0200

    updated calculations of delta with yesterday

--- Pages/Covid19.cshtml.cs
+++ Pages/Covid19.cshtml.cs
@@ -14,6 +14,7 @@ namespace p_tenz.Covid19Stats.WebApp.Pages
         private readonly ILogger<Covid19Model> _logger;
 
         public string Title { get; internal set; }
+        public string SubTitle { get; internal set; }
         public List<CasesModels.Feature> covid19Data { get; set; }
 
         public Covid19Model(ILogger<Covid19Model> logger)
@@ -29,7 +30,8 @@ namespace p_tenz.Covid19Stats.WebApp.Pages
             this.Title = "Covid-19 Fälle in Stuttgart";
 
             covid19Data = await CasesController.LoadCovidData();
-            var newCases = covid19Data.Sum(f => f.attributes.NeuerFall);
+            this.SubTitle = covid19Data.FirstOrDefault().attributes.Datenstand;
+            
 
             return Page();
         }
